95b0ba12572847968a9a331ef5b94ef73cfe170a
Ash Trindade <ashtrindade5@gmail.com>
2024-09-29 16:47:24 -0300

    chore: update types

--- src/types/Log.ts
+++ src/types/Log.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type Tag =
-  'controller'
-  | 'service'
-  | 'application'
-  | 'database'
-  | 'middleware'
-  | 'error'
-  | 'error_handler'
-  | 'router'
-  | 'auth'
-  | 'cron'
+    'controller'
+    | 'service'
+    | 'application'
+    | 'database'
+    | 'middleware'
+    | 'error'
+    | 'error_handler'
+    | 'router'
+    | 'auth'
+    | 'cron'
 
 export type LogInfo = {
-  message: string
-  tag: Tag
-  timestamp: string
-  level: 'debug' | 'info' | 'warn' | 'error'
-  details?: string
-  stacktrace?: any
+    message: string
+    tag: Tag
+    timestamp: string
+    level: 'debug' | 'info' | 'warn' | 'error'
+    details?: string
+    stacktrace?: any
 }
